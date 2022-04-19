7078575df66433abb24710b6f584f1e79189d79a
Nathan Benkemoun <nathan.ben-kemoun@student.isae-supaero.fr>
2022-04-19 11:29:18 +0200

    Create Itineraire model
    Also give admin access to this model

--- topoguide/itineraires/models.py
+++ topoguide/itineraires/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Itineraire(models.Model):
+    """
+    A single itinerary available
+    """
+    name = models.CharField('titre',max_length=200)
+    start_point = models.CharField('point de départ',max_length=200)
+    description = models.CharField(max_length=400)
+    start_alt = models.FloatField('altitude de départ (m)') 
+    alt_min = models.FloatField('altitude min (m)') 
+    alt_max = models.FloatField('altitude max (m)') 
+    elevation_gain = models.FloatField('dénivelé positif cumulé (m)')
+    elevation_loss = models.FloatField('dénivelé négatif cumulé (m)')
+    duration = models.FloatField('durée estimée (h)')
+    DIF_CHOICE = (
+        (1,'1'),
+        (2,'2'),
+        (3,'3'),
+        (4,'4'),
+        (5,'5'),
+    )
+    difficulty = models.IntegerField('difficulté', default=1, choices=DIF_CHOICE)
+    
+    
+    def __str__(self):
+        return self.name
+
