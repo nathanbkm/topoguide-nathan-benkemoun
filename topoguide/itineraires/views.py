ea80296c569d5847f2743a0c37983cf2444ba486
Nathan Benkemoun <nathan.ben-kemoun@student.isae-supaero.fr>
2022-04-21 09:18:48 +0200

    Add a view modif_sortie and a corresponding path

--- topoguide/itineraires/views.py
+++ topoguide/itineraires/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import get_list_or_404, get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
 
@@ -25,8 +26,9 @@ def sorties(request, itineraire_id):
     """
     sorties = get_list_or_404(Sortie, itineraire_id=itineraire_id )
     itineraire = get_object_or_404(Itineraire, pk=itineraire_id)
+    utilisateur = request.user
     return render(request, 'itineraires/sorties.html',
-                  {'sorties': sorties, 'itineraire': itineraire})
+                  {'sorties': sorties, 'itineraire': itineraire, 'utilisateur':utilisateur})
 
 @login_required()
 def sortie(request, sortie_id):
@@ -62,4 +64,34 @@ def nouvelle_sortie(request, itineraire_id):
             sortie.save()
             return redirect('itineraires:sortie', sortie.id)
     itineraire = get_object_or_404(Itineraire, pk=itineraire_id )
-    return render(request, 'itineraires/nouvelle_sortie.html', {'form': form, 'itineraire': itineraire})
+    return render(request, 'itineraires/edition_sortie.html', {'form': form, 'itineraire': itineraire})
+
+@login_required
+def modif_sortie(request, sortie_id):
+    """
+    Modify an existing trip based on user input in form
+    Args:
+        request: the incoming request, GET or POST
+        sortie_id: the trip's ID of the trip to update
+    Returns:
+        - a page with a pre-filled form if it was a GET request,
+        - a page with a pre-filled form if it was a POST request
+          with invalid data,
+        - or the page of the input trip if it was a POST with valid data
+    """
+    sortie = get_object_or_404(Sortie, pk=sortie_id)
+    
+    if sortie.utilisateur != request.user:
+        return HttpResponse("Vous ne pouvez modifier que les sorties que vous avez créé.")
+    elif request.method == 'GET':
+        form = TripForm(instance=sortie)
+    elif request.method == 'POST':
+        form = TripForm(request.POST, instance=sortie)
+        if form.is_valid():
+            sortie_mod = form.save(commit=False)
+            sortie_mod.utilisateur = request.user
+            sortie_mod.itineraire = get_object_or_404(Itineraire, pk=sortie.itineraire.id)
+            sortie.save()
+            form.save()
+            return redirect('itineraires:sortie', sortie.id)
+    return render(request, 'itineraires/edition_sortie.html', {'form': form, 'itineraire': sortie.itineraire})
